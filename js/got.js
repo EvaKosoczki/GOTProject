c9bae0456354840ebbc8118f281fa13b61a45499
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-07-18 21:23:01 +0200

    isAlive function working

--- js/got.js
+++ js/got.js
@@ -1,7 +1,7 @@
-var userDatas;
+let userDatas;
 
 function getData(url, callbackFunc) {
-  var xhttp = new XMLHttpRequest();
+  const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function getCharacters() {
     if (this.readyState === 4 && this.status === 200) {
       callbackFunc(this);
@@ -13,9 +13,29 @@ function getData(url, callbackFunc) {
 
 function successAjax(xhttp) {
   userDatas = JSON.parse(xhttp.responseText);
+  isAlive(userDatas);
 }
 
-getData('/json/aJsonFileodNeve.json', successAjax);
+getData('/json/got.json', successAjax);
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+function isAlive(data) {
+  const stillAlive = [];
+  let result;
+  for (let i = 0; i < data.length; i += 1) {
+    const keys = Object.keys(data[i]);
+    for (let j = 0; j < keys.length; j += 1) {
+      if (keys[j] !== 'dead') {
+        result = true;
+      } else {
+        result = false;
+        break;
+      }
+    }
+    if (result) {
+      stillAlive.push(data[i]);
+    }
+  }
+  return stillAlive;
+}
